143e07fdb867423595e005eaddc7db50e98032b5
dp-beep <1091237067@qq.com>
2022-04-20 20:11:06 +0800

    Initial commit

---
+++ entry/src/main/java/com/example/flexlayout/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.flexlayout;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
